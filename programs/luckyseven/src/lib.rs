8788620f868a464821f32ae2ee037ab485085187
Matías Barrios <matias.barriosn@gmail.com>
2022-03-04 12:53:13 -0300

    feat: initializing storage account

--- programs/luckyseven/src/lib.rs
+++ programs/luckyseven/src/lib.rs
@@ -1,15 +1,87 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::system_program;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+const MAX_VALUE: i64 = 1234;
 
 #[program]
 pub mod luckyseven {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let program_storage: &mut Account<ProgramStorage> = &mut ctx.accounts.program_storage;
+        let deployer: &mut Signer = &mut ctx.accounts.owner;
+        program_storage.initialized = true;
+        program_storage.last_difference = MAX_VALUE;
+        program_storage.game_owner = *deployer.key;
+
+        Ok(())
+    }
+
+    pub fn store_number(ctx: Context<StoreNumber>) -> Result<()> {
+        let program_storage: &mut Account<ProgramStorage> = &mut ctx.accounts.program_storage;
+        require!(program_storage.initialized, ErrorCode::NotInitializedYet);
+        let random_number: &mut Account<RandomNumber> = &mut ctx.accounts.random_number;
+        let owner: &Signer = &ctx.accounts.owner;
+        let clock: Clock = Clock::get().unwrap();
+        random_number.number = clock.unix_timestamp % MAX_VALUE;
+        random_number.owner = *owner.key;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = owner)]
+    pub program_storage: Account<'info, ProgramStorage>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    #[account(address = system_program::ID)]
+    /// CHECK: this is not unsafe because we check that the account is indeed system_program
+    pub system_program: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+pub struct StoreNumber<'info> {
+    #[account(init, payer = owner, space = RandomNumber::LEN)]
+    pub random_number: Account<'info, RandomNumber>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    #[account(mut)]
+    pub program_storage: Account<'info, ProgramStorage>,
+    #[account(address = system_program::ID)]
+    /// CHECK: this is not unsafe because we check that the account is indeed system_program
+    pub system_program: AccountInfo<'info>,
+}
+
+#[account]
+pub struct RandomNumber {
+    pub owner: Pubkey,
+    pub number: i64,
+    pub winner: bool,
+}
+
+#[account]
+#[derive(Default)]
+pub struct ProgramStorage {
+    pub last_difference: i64,
+    pub initialized: bool,
+    pub game_owner: Pubkey,
+}
+
+const DISCRIMINATOR_LENGTH: usize = 8;
+const OWNER_LENGTH: usize = 32;
+const NUMBER_LENGTH: usize = 32;
+
+impl RandomNumber {
+    const LEN: usize = DISCRIMINATOR_LENGTH + OWNER_LENGTH + NUMBER_LENGTH;
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Already initialized.")]
+    AlreadyInitialized,
+    #[msg("Not initialized yet.")]
+    NotInitializedYet,
+}
--- tests/luckyseven.ts
+++ tests/luckyseven.ts
@@ -7,10 +7,44 @@ describe("luckyseven", () => {
   anchor.setProvider(anchor.Provider.env());
 
   const program = anchor.workspace.Luckyseven as Program<Luckyseven>;
+  const programStorage = anchor.web3.Keypair.generate();
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
+  it("is initialized!", async () => {
+    const tx = await program.rpc.initialize({
+      accounts: {
+        programStorage: programStorage.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+        owner: program.provider.wallet.publicKey,
+      },
+      signers: [programStorage],
+    });
     console.log("Your transaction signature", tx);
+    await program.provider.connection.confirmTransaction(tx);
+    const { initialized, lastDifference, gameOwner } =
+      await program.account.programStorage.fetch(programStorage.publicKey);
+    console.log(initialized);
+    console.log(lastDifference.toString());
+    console.log(gameOwner.toBase58());
+  });
+
+  it("stores a random number", async () => {
+    const randomNumber = anchor.web3.Keypair.generate();
+    // Add your test here.
+    const tx = await program.rpc.storeNumber({
+      accounts: {
+        programStorage: programStorage.publicKey,
+        owner: program.provider.wallet.publicKey,
+        randomNumber: randomNumber.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [randomNumber],
+    });
+    console.log("Your transaction signature", tx);
+
+    const { owner, number } = await program.account.randomNumber.fetch(
+      randomNumber.publicKey
+    );
+    console.log(owner.toBase58());
+    console.log(number.toString());
   });
 });
